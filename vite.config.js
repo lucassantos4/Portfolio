d291c15a82cc03713787f561663d082658dd627e
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-04-10 21:02:36 -0300

    feat: adicionar suporte ao sitemap com vite-plugin-sitemap e remover sitemap.xml

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://lucasnsnt.ink',
+      dynamicRoutes: ['/'],
+      lastmod: new Date(), 
+    }),
+  ],
   base: "/",
 })
